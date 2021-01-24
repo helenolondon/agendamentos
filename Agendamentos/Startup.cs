9f82847b64873d472b5c31f17cea7c0ad0e8adc0
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2021-01-23 21:24:05 -0300

    Entity Framework atualizado para a última versão Json identado configurado

--- Agendamentos/Startup.cs
+++ Agendamentos/Startup.cs
@@ -23,7 +23,11 @@ namespace Agendamentos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddJsonOptions( options => 
+                {
+                    options.JsonSerializerOptions.WriteIndented = true;   
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
